fd7a72eed26fdea10c168eeb862a5cfa79255d3a
robertosanchez11 <roberto.sanchez11@upr.edu>
2022-02-17 08:34:21 -0400

    cin name

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <String>
 
 using namespace std;
 
 int main() {
+    string name;
+
     cout << "Hello World" << endl;
+    cout << "Your name is:" << endl;
+    cin >> name;
+    cout << "Hello" << name << endl;
     return 0;
 }
